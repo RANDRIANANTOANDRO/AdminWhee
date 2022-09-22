617d45b6a6151beff31a52f8fa9bb5a3cad16314
Joel <joelandriambola@gmail.com>
2022-09-22 10:24:27 +0300

    Ajout des services

--- Front-admin/src/components/table/Table.jsx
+++ Front-admin/src/components/table/Table.jsx
@@ -6,7 +6,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-
+imp
 const List = () => {
   const rows = [
     {
